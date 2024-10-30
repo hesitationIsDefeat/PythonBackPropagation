79043f08ec7f0a1e64a92445a595b8664e6360b0
Onat Tanrıöver <onat@Onats-MacBook-Pro.local>
2024-10-30 16:38:48 +0300

    Finished neuron parameter update testing

--- tests/test_neuron.py
+++ tests/test_neuron.py
@@ -1,20 +1,23 @@
 import unittest
 import numpy as np
-from numpy.ma.testutils import assert_equal
-
+from numpy.testing import assert_allclose
 from back_propagation.classes.neuron import Neuron
+
+is_skipping_single_neuron_connections = True
+is_skipping_neuron_parameters_update = False
 
 INPUT_NEURON_ACTIVATION = 5
 
-class TestSingleNeuron(unittest.TestCase):
+@unittest.skipIf(is_skipping_single_neuron_connections, "Skipping TestSingleNeuronConnections")
+class TestSingleNeuronConnections(unittest.TestCase):
     def setUp(self):
         self.single_input_neuron = Neuron()
         self.single_input_neuron.set_input_neuron_activation(INPUT_NEURON_ACTIVATION)
 
-        self.single_hidden_neuron = Neuron(gradient_starting_index=0, connections=[self.single_input_neuron])
+        self.single_hidden_neuron = Neuron(connections=[self.single_input_neuron])
         self.single_hidden_neuron.calculate_activation_value()
 
-        self.single_output_neuron = Neuron(gradient_starting_index=2, connections=[self.single_hidden_neuron])
+        self.single_output_neuron = Neuron(connections=[self.single_hidden_neuron])
         self.single_output_neuron.calculate_activation_value()
 
 
@@ -39,4 +42,66 @@ class TestSingleNeuron(unittest.TestCase):
         self.assertEqual(self.single_input_neuron.activation_value, INPUT_NEURON_ACTIVATION)
         self.assertEqual(self.single_hidden_neuron.activation_value, hidden_neuron_activation)
         self.assertEqual(self.single_output_neuron.activation_value, output_neuron_activation)
+
+
+INPUT_LAYER_SIZE = 3
+FIRST_HIDDEN_LAYER_SIZE = 5
+SECOND_HIDDEN_LAYER_SIZE = 7
+OUTPUT_LAYER_SIZE = 5
+
+INPUT_NEURON_ACTIVATIONS = np.random.rand(INPUT_LAYER_SIZE)
+
+GRADIENT_DESCEND_VECTOR_PARTS = [np.random.rand(INPUT_LAYER_SIZE + 1),
+                                 np.random.rand(FIRST_HIDDEN_LAYER_SIZE + 1),
+                                 np.random.rand(SECOND_HIDDEN_LAYER_SIZE + 1)]
+
+@unittest.skipIf(is_skipping_neuron_parameters_update, "Skipping TestNeuronParameterUpdate")
+class TestNeuronParameterUpdate(unittest.TestCase):
+    def setUp(self):
+        self.input_layer = [Neuron() for _ in range(INPUT_LAYER_SIZE)]
+        for index in range(INPUT_LAYER_SIZE):
+            self.input_layer[index].set_input_neuron_activation(INPUT_NEURON_ACTIVATIONS[index])
+
+        self.first_hidden_layer = [Neuron(connections=self.input_layer) for _ in range(FIRST_HIDDEN_LAYER_SIZE)]
+        for neuron in self.first_hidden_layer:
+            neuron.calculate_activation_value()
+
+        self.second_hidden_layer = [Neuron(connections=self.first_hidden_layer) for _ in range(SECOND_HIDDEN_LAYER_SIZE)]
+        for neuron in self.second_hidden_layer:
+            neuron.calculate_activation_value()
+
+        self.output_layer = [Neuron(connections=self.second_hidden_layer) for _ in range(OUTPUT_LAYER_SIZE)]
+        for neuron in self.output_layer:
+            neuron.calculate_activation_value()
+
+    def assertParamsEqual(self, single_neuron, gradient_descend_vector_part_index):
+        gradient_descend_vector_part = GRADIENT_DESCEND_VECTOR_PARTS[gradient_descend_vector_part_index]
+        updated_weights = single_neuron._weights - gradient_descend_vector_part[:-1]
+        updated_bias = single_neuron._bias - gradient_descend_vector_part[-1]
+
+        single_neuron.update_params(gradient_descend_vector_part)
+
+        assert_allclose(single_neuron._weights, updated_weights)
+        self.assertEqual(single_neuron._bias, updated_bias)
+
+    @unittest.skip
+    def test_single_hidden_neuron_parameter_update(self):
+        self.assertParamsEqual(self.first_hidden_layer[0], 0)
+
+    @unittest.skip
+    def test_first_hidden_layer_parameter_update(self):
+        for single_hidden_neuron in self.first_hidden_layer:
+            self.assertParamsEqual(single_hidden_neuron, 0)
+
+    def test_all_layers_parameter_update(self):
+        for single_hidden_neuron in self.first_hidden_layer:
+            self.assertParamsEqual(single_hidden_neuron, 0)
+
+        for single_hidden_neuron in self.second_hidden_layer:
+            self.assertParamsEqual(single_hidden_neuron, 1)
+
+        for single_output_neuron in self.output_layer:
+            self.assertParamsEqual(single_output_neuron, 2)
+
+
 
